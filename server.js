10014debe8c1e13d680ba7c5a01e98fcae45d0be
Barrett <baxance@protonmail.com>
2021-03-09 11:02:13 -0800

    images rendering on

--- server.js
+++ server.js
@@ -19,14 +19,16 @@ app.set('view engine', 'ejs');
 
 //Database setup
 const client = new pg.Client(process.env.DATABASE_URL);
+const UNSPLASH_KEY = process.env.UNSPLASH_KEY
 
 //Routes
 app.get('/', displayHomePage);
 app.get('/userboard', displayUserboard);
 app.get('/details/:user/:id', displayDetails);
 app.get('/about_us', displayAboutUs);
+app.get('/saves', displaySearchResults)
 
-app.post('/categories', conductSearch);
+// app.post('/categories', conductSearch);
 app.post('/newentry', addToBoard);
 
 app.delete('/details/:user/:id', deleteEntry);
@@ -45,8 +47,15 @@ function displayDetails(req, res) {
   res.render('./details.ejs')
 }
 
-function conductSearch(req, res) {
-  
+function displaySearchResults(req, res) {
+  const url = `https://api.unsplash.com/search/photos?query=${req.body.query}&client_id=${UNSPLASH_KEY}`;
+  superagent.get(url)
+    .then(results => {
+      const visionList = new createVisionList(results.body.results);
+      console.log(visionList);
+      const ejsObject = {visionList};
+      res.render('./saves.ejs', ejsObject);
+    })
 }
 
 function addToBoard(req, res) {
@@ -71,6 +80,12 @@ function Vision (username, image_url, description, goal_deadline) {
   this.image_url - image_url;
   this.description = description;
   this.goal_deadline = goal_deadline;
+}
+
+function createVisionList (object) {
+  return object.map(image => {
+  return image.urls.regular;
+  })
 }
 
 client.connect()
